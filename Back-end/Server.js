209140ea1fedd628c3a7c3abd20194448632d21a
Wea'am <84277488+Weaam20@users.noreply.github.com>
2023-06-02 10:53:59 +0300

    Update Server.js

--- Back-end/Server.js
+++ Back-end/Server.js
@@ -1,6 +1,9 @@
 const zip_path = require.resolve('./assets/js/Main');
 const zip = require(zip_path);
 
+const message_booking = "";
+const message_canclation = "";
+
 const nodemailer = require("nodemailer");
 const mailgen = require("mailgen");
 
@@ -157,67 +160,14 @@ app.post('/booking', async function(req, res) {
 
 		const reservation = await Reservation.create({'day': day, 'time': time, "code": code, "Section": Section, "Status": Status, "teacher_id": teacherID, "student_id": studentID});
 		
-		const student = await Student.find({_id : studentID});
-		console.log(student);
-		const name = student.user;
-		let student_name = name.Fname;
-		console.log(student_name);
+		let student = await Student.find({_id : studentID});
+		console.log(student[0]);
 		
-//===========
-		
-	const userEmail  = "xanope8975@ratedane.com";
-
-    let config = {
-        service : 'gmail',
-        auth : {
-            user: 'imam.cs.oh@gmail.com',
-            pass: 'f p g c o n k s d n n l t w x i'
-        }
-    }
-
-    let transporter = nodemailer.createTransport(config);
-
-    let MailGenerator = new mailgen({
-        theme: "default",
-        product : {
-            name: "ساعة مكتبية ",
-            link : 'https://mailgen.js/'
-        }
-    })
-
-    let response = {
-        body: {
-            name : "Dr. Basma",
-            intro: "حجز موعد",
-            table : {
-                data : [
-                    {
-                        "أسم الطالب/ة" : "Nodemailer Stack Book",
-                        "المقرر": "A Backend application",
-						"الشعبة": "A Backend application",
-                        "اليوم" : "$10.99",
-						"الوقت" : "$10.99",
-						"بريد الطالب" : "$10.99",
-                    }
-                ]
-            },
-            outro: "Looking forward to do more business"
-        }
-    }
-
-    let mail = MailGenerator.generate(response)
-
-    let message = {
-        from : 'imam.cs.oh@gmail.com',
-        to : userEmail,
-        subject: "تطبيق ساعة مكتبية (Office Hour Application)",
-        html: mail
-    }
-
-    transporter.sendMail(message).catch(error => {
-        return res.status(500).json({ error })
-    })
-//===========		
+		student = student[0];
+		//const name = ;
+		//let student_name = ;
+		const student_full_name = student.user.Fname +" "+ student.user.Sname +" "+ student.user.Tname +" "+ student.user.Lname;
+		const student_email = student.user.email;
 		
 		Reservation.find({}).then( async function(reservations){
 		
@@ -240,6 +190,224 @@ app.post('/booking', async function(req, res) {
 		for (const prop in teacher_names) {
 			console.log(`key: ${prop}, value: ${teacher_names[prop]}`);
 		}
+	
+//===========
+		
+	const userEmail  = "wksalghaith@sm.imamu.edu.sa";
+
+    let config = {
+        service : 'gmail',
+        auth : {
+            user: 'imam.cs.oh@gmail.com',
+            pass: 'f p g c o n k s d n n l t w x i'
+        }
+    }
+
+    let transporter = nodemailer.createTransport(config);
+
+    let MailGenerator = new mailgen({
+        theme: "default",
+        product : {
+            name: "ساعة مكتبية ",
+            link : 'https://mailgen.js/'
+        }
+    })
+
+    let response = {
+        body: {
+            name : teacher_names[teacherID],
+            intro: "تم حجز موعد في ساعتك المكتبية",
+            table : {
+                data : [
+                    {
+                        "أسم الطالب/ة" : student_full_name,
+                        "المقرر": code,
+						"الشعبة": Section,
+                        "اليوم" : day,
+						"الوقت" : time,
+						"بريد الطالب" : student_email,
+						
+                    }
+                ]
+            },
+            outro: "شاكرين لك وقتك"
+        }
+    }
+
+    let mail = MailGenerator.generate(response)
+	
+	console.log(mail);
+
+    let message =  
+	{
+        from : 'imam.cs.oh@gmail.com',
+        to : userEmail,
+        subject: "تطبيق ساعة مكتبية (Office Hour Application)",
+        html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <style type="text/css" rel="stylesheet" media="all">
+@media only screen and (max-width: 600px) {
+  .email-body_inner,
+.email-footer {
+    width: 100% !important;
+  }
+}
+@media only screen and (max-width: 500px) {
+  .button {
+    width: 100% !important;
+  }
+}
+</style>
+</head>
+<body dir="ltr" style="height: 100%; margin: 0; line-height: 1.4; background-color: #F2F4F6; color: #74787E; -webkit-text-size-adjust: none; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; width: 100%;">
+  <table class="email-wrapper" width="100%" cellpadding="0" cellspacing="0" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; width: 100%; margin: 0; padding: 0; background-color: #F2F4F6;" bgcolor="#F2F4F6">
+    <tr>
+      <td align="center" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box;">
+        <table class="email-content" width="100%" cellpadding="0" cellspacing="0" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; width: 100%; margin: 0; padding: 0;">
+          <!-- Logo -->
+          <tr>
+            <td class="email-masthead" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; padding: 25px 0; text-align: center;" align="center">
+              <a class="email-masthead_name" href="https://mailgen.js/" target="_blank" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; font-size: 16px; font-weight: bold; color: #2F3133; text-decoration: none; text-shadow: 0 1px 0 white;">
+
+                  ساعة مكتبية
+
+                </a>
+            </td>
+          </tr>
+          <!-- Email Body -->
+          <tr>
+            <td class="email-body" width="100%" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; width: 100%; margin: 0; padding: 0; border-top: 1px solid #EDEFF2; border-bottom: 1px solid #EDEFF2; background-color: #FFF;" bgcolor="#FFF">
+              <table class="email-body_inner" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; width: 570px; margin: 0 auto; padding: 0;">
+                <!-- Body content -->
+                <tr>
+                  <td class="content-cell" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; padding: 35px;">
+
+                      <h1 style="margin-top: 0; color: #2F3133; font-size: 19px; font-weight: bold; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box;">استاذنا العزيز نفيدك علماً أنه</h1>
+
+
+                                              <p style="margin-top: 0; color: #74787E; font-size: 16px; line-height: 1.5em; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box;">تم حجز موعد في ساعتك المكتبية</p>
+
+
+                    <!-- Dictionary -->
+
+
+                    <!-- Table -->
+
+                                            <h1 class="data-table-title" style="margin-top: 0; color: #2F3133; font-weight: bold; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; width: 100%; margin: 0; font-size: 17px; padding: 20px 0 15px 0;"></h1>
+                      <table class="data-wrapper" width="100%" cellpadding="0" cellspacing="0" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; width: 100%; margin: 0; padding: 0 0 35px 0;">
+                        <tr>
+                          <td colspan="2" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box;">
+                            <table class="data-table" width="100%" cellpadding="0" cellspacing="0" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; width: 100%; margin: 0;">
+                              <tr>
+
+                                  <th style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; text-align: left; padding: 0px 5px; padding-bottom: 8px; border-bottom: 1px solid #EDEFF2;" align="left">
+                                    <p style="margin-top: 0; line-height: 1.5em; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; margin: 0; color: #9BA2AB; font-size: 12px;">أسم الطالب/ة</p>
+                                  </th>
+
+                                  <th style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; text-align: left; padding: 0px 5px; padding-bottom: 8px; border-bottom: 1px solid #EDEFF2;" align="left">
+                                    <p style="margin-top: 0; line-height: 1.5em; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; margin: 0; color: #9BA2AB; font-size: 12px;">المقرر</p>
+                                  </th>
+
+                                  <th style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; text-align: left; padding: 0px 5px; padding-bottom: 8px; border-bottom: 1px solid #EDEFF2;" align="left">
+                                    <p style="margin-top: 0; line-height: 1.5em; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; margin: 0; color: #9BA2AB; font-size: 12px;">الشعبة</p>
+                                  </th>
+
+                                  <th style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; text-align: left; padding: 0px 5px; padding-bottom: 8px; border-bottom: 1px solid #EDEFF2;" align="left">
+                                    <p style="margin-top: 0; line-height: 1.5em; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; margin: 0; color: #9BA2AB; font-size: 12px;">اليوم</p>
+                                  </th>
+
+                                  <th style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; text-align: left; padding: 0px 5px; padding-bottom: 8px; border-bottom: 1px solid #EDEFF2;" align="left">
+                                    <p style="margin-top: 0; line-height: 1.5em; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; margin: 0; color: #9BA2AB; font-size: 12px;">الوقت</p>
+                                  </th>
+
+                                  <th style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; text-align: left; padding: 0px 5px; padding-bottom: 8px; border-bottom: 1px solid #EDEFF2;" align="left">
+                                    <p style="margin-top: 0; line-height: 1.5em; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; margin: 0; color: #9BA2AB; font-size: 12px;">بريد الطالب</p>
+                                  </th>
+
+                              </tr>
+
+                              <tr>
+
+                                  <td style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; padding: 10px 5px; color: #74787E; font-size: 15px; line-height: 18px;">
+                                    ` + student_full_name + `
+                                  </td>
+
+                                  <td style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; padding: 10px 5px; color: #74787E; font-size: 15px; line-height: 18px;">
+                                    ` + code + `
+                                  </td>
+
+                                  <td style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; padding: 10px 5px; color: #74787E; font-size: 15px; line-height: 18px;">
+                                    ` + Section + `
+                                  </td>
+
+                                  <td style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; padding: 10px 5px; color: #74787E; font-size: 15px; line-height: 18px;">
+                                    ` + day + `
+                                  </td>
+
+                                  <td style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; padding: 10px 5px; color: #74787E; font-size: 15px; line-height: 18px;">
+                                    ` + time + `
+                                  </td>
+
+                                  <td style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; padding: 10px 5px; color: #74787E; font-size: 15px; line-height: 18px;">
+                                    ` + student_email + `
+                                  </td>
+
+                              </tr>
+
+                            </table>
+                          </td>
+                        </tr>
+                      </table>
+
+
+
+                    <!-- Action -->
+
+
+                    <!-- Support for Gmail Go-To Actions -->
+
+
+
+                                              <p style="margin-top: 0; color: #74787E; font-size: 16px; line-height: 1.5em; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box;">شاكرين لك وقتك</p>
+
+
+
+                      <p style="margin-top: 0; color: #74787E; font-size: 16px; line-height: 1.5em; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box;"></p>
+
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+
+          <tr>
+            <td style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box;">
+              <table class="email-footer" align="center" width="570" cellpadding="0" cellspacing="0" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; width: 570px; margin: 0 auto; padding: 0; text-align: center;">
+                <tr>
+                  <td class="content-cell" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; padding: 35px;">
+                    <p class="sub center" style="margin-top: 0; line-height: 1.5em; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box; color: #AEAEAE; font-size: 12px; text-align: center;">
+                      &copy; 2023 <a href="https://mailgen.js/" target="_blank" style="color: #3869D4; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; -webkit-box-sizing: border-box; box-sizing: border-box;">ساعة مكتبية </a>. All rights reserved.
+                    </p>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>`
+    }
+	
+    transporter.sendMail(message).catch(error => {
+        return res.status(500).json({ error })
+    })
+//===========		
 		
 
 		res.render('mybooking', {reserv_list: reservations, teacher_id_names:  teacher_names}).catch(function(err){console.log(err);})
