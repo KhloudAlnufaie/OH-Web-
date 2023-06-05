a72f322bc2dc634a39f4d16f35af14444844dd22
Wea'am <84277488+Weaam20@users.noreply.github.com>
2023-06-05 23:56:41 +0300

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -1,1 +1,50 @@
 
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema(
+
+	{
+	user: {
+		Fname: {
+			type: String,
+			required: [true, "الرجاء إدخال الاسم الأول"],
+		},
+		Sname: {
+			type: String,
+			required: [true, "الرجاء إدخال الاسم الثاني"],
+		},
+		Tname: {
+			type: String,
+			required: [true, "الرجاء إدخال الاسم الثالث"],
+		},
+		Lname: {
+			type: String,
+			required: [true, "الرجاء إدخال اسم العائلة"],
+		},
+		email: {
+			type: String,
+			required:  [true, "الرجاء إدخال البريد الإلكتروني"],
+		}
+	},
+	password: {
+		type: String,
+		required:  [true, "الرجاء إدخال كلمة السر"],
+	},
+	ReservationList: {
+		type: Array,
+		required:  false,
+	}
+	
+	},
+	{
+		timestamps: true
+	}
+);
+  
+  const StudentSchema = mongoose.model("Student", Schema);
+  
+ // export const schema = Student.schema;
+  export default StudentSchema;
+ // module.exports = Student;
+  
+  
+		
